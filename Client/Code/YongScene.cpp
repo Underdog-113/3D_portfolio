026beecc750915e5dad795e4ffcc9590e3fa9bc7
Gyong <fu79899@gmail.com>
2021-06-04 09:33:37 +0900

    텍스처 추가

--- Client/Code/YongScene.cpp
+++ Client/Code/YongScene.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "YongScene.h"
-
+#include "EmptyObject.h"
 
 CYongScene::CYongScene()
 {
@@ -33,6 +33,19 @@ void CYongScene::Awake(_int numOfLayers)
 
 void CYongScene::Start(void)
 {
+	__super::Start();
+	{
+		SP(Engine::CObject) spEmptyObject
+			= m_pObjectFactory->AddClone(L"EmptyObject", true, (_int)ELayerID::Player, L"Cube0");
+
+		spEmptyObject->AddComponent<Engine::CMeshC>()->AddMeshData(L"Yae_Sakura2");
+		spEmptyObject->AddComponent<Engine::CTextureC>();
+		spEmptyObject->GetComponent<Engine::CMeshC>()->SetInitTex(true);
+		spEmptyObject->AddComponent<Engine::CGraphicsC>()->SetRenderID((_int)Engine::ERenderID::NonAlpha);
+
+
+		
+	}
 }
 
 void CYongScene::FixedUpdate(void)
@@ -43,6 +56,32 @@ void CYongScene::FixedUpdate(void)
 void CYongScene::Update(void)
 {
 	__super::Update();
+	SP(Engine::CObject) momBox = FindObjectByName(L"Cube0");
+
+	if (Engine::IMKEY_PRESS(KEY_UP))
+	{
+		momBox->GetTransform()->MoveForward(0.05f);
+	}
+	if (Engine::IMKEY_PRESS(KEY_LEFT))
+	{
+		momBox->GetTransform()->AddRotationY(-0.05f);
+	}
+	if (Engine::IMKEY_PRESS(KEY_RIGHT))
+	{
+		momBox->GetTransform()->AddRotationY(0.05f);
+	}
+	if (Engine::IMKEY_PRESS(KEY_DOWN))
+	{
+		momBox->GetTransform()->AddRotationX(0.05f);
+	}
+	if (Engine::IMKEY_PRESS(KEY_E))
+	{
+		momBox->GetTransform()->AddRotationZ(0.05f);
+	}
+	if (Engine::IMKEY_DOWN(KEY_Q))
+	{
+		momBox->SetDeleteThis(true);
+	}
 	
 }
 
