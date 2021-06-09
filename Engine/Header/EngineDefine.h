585ce847405431b97071dd3c99c12d51e3c31ad3
urisian <yjcm1214@gmail.com>
2021-06-10 00:56:02 +0900

    키 갈아치움

--- Engine/Header/EngineDefine.h
+++ Engine/Header/EngineDefine.h
@@ -20,48 +20,50 @@
 //UnusedParam
 #define UNUSED(x) (void)(x)
 
-#define KEY_LEFT	1 << 0
-#define KEY_RIGHT	1 << 1
-#define KEY_UP		1 << 2
-#define KEY_DOWN	1 << 3
-#define KEY_SPACE	1 << 4
-#define KEY_CONTROL	1 << 5
-#define MOUSE_LEFT	1 << 6
-#define MOUSE_RIGHT	1 << 7
-#define MOUSE_WHEEL	1 << 8
-#define KEY_F1		1 << 9
-#define KEY_F2		1 << 10
-#define KEY_F3		1 << 11
-#define KEY_F4		1 << 12
-#define KEY_F5		1 << 13
-#define KEY_W		1 << 14
-#define KEY_A		1 << 15
-#define KEY_S		1 << 16
-#define KEY_D		1 << 17
-#define KEY_Q		1 << 18
-#define KEY_E		1 << 19
-#define KEY_1       1 << 20
-#define KEY_2       1 << 21
-#define KEY_3		1 << 22
-#define KEY_4		1 << 23
-#define KEY_5		1 << 24
-#define KEY_TAB		1 << 25
-#define KEY_SHIFT	1 << 26
-#define KEY_Z		1 << 27
-#define KEY_X		1 << 28
-#define KEY_DELETE	1 << 29
 
-#define KEY_6		1 << 0
-#define KEY_7		1 << 1
-#define KEY_8		1 << 2
-#define KEY_F6		1 << 3
-#define KEY_F7		1 << 4
-#define KEY_F8		1 << 5
-#define KEY_F		1 << 6
-#define KEY_U		1 << 7
-#define KEY_I		1 << 8
-#define KEY_J		1 << 9
-#define KEY_K		1 << 10
+#define KEY_LEFT	1ULL << 0 
+#define KEY_RIGHT	1ULL << 1
+#define KEY_UP		1ULL << 2
+#define KEY_DOWN	1ULL << 3
+#define KEY_WAVE	1ULL << 4
+#define KEY_TAB		1ULL << 5
+#define KEY_SHIFT	1ULL << 6
+#define KEY_CONTROL	1ULL << 7
+#define KEY_SPACE	1ULL << 8
+#define KEY_RETURN	1ULL << 9
+#define KEY_ESC		1ULL << 10
+#define MOUSE_LEFT	1ULL << 11
+#define MOUSE_RIGHT	1ULL << 12
+#define MOUSE_WHEEL	1ULL << 13
+#define KEY_1		1ULL << 14
+#define KEY_2		1ULL << 15
+#define KEY_3		1ULL << 16
+#define KEY_4		1ULL << 17
+#define KEY_5		1ULL << 18
+#define KEY_F1		1ULL << 19
+#define KEY_F2		1ULL << 20
+#define KEY_F3		1ULL << 21
+#define KEY_F4		1ULL << 22
+#define KEY_F5		1ULL << 23
+#define KEY_Q		1ULL << 24
+#define KEY_W		1ULL << 25
+#define KEY_E		1ULL << 26
+#define KEY_R		1ULL << 27
+#define KEY_A		1ULL << 28
+#define KEY_S		1ULL << 29
+#define KEY_D		1ULL << 30
+#define KEY_F		1ULL << 31
+#define KEY_Z		1ULL << 32
+#define KEY_X		1ULL << 33
+#define KEY_C		1ULL << 34
+#define KEY_V		1ULL << 35
+#define KEY_U		1ULL << 36
+#define KEY_I		1ULL << 37
+#define KEY_J		1ULL << 38
+#define KEY_K		1ULL << 39
+
+
+
 
 /*
 #define MOUSE_LEFT		1 << 0
