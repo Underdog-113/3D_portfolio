5b3da49e060e54c4de74fbe7f5ca4d7ecdf59ccd
lee <asqqwww1@gmail.com>
2021-06-15 17:55:03 +0900

    remove conflict

--- Engine/Code/CatPawShader.cpp
+++ Engine/Code/CatPawShader.cpp
@@ -66,7 +66,7 @@ void CCatPawShader::SetUpConstantTable(SP(CGraphicsC) spGC)
 
 
 	SP(CTextureC) spTexture = spGC->GetTexture();
-	m_pEffect->SetTexture("g_SubTex", spTexture->GetTexData()[spTexture->GetMeshIndex()][1]->pTexture);
+	/*m_pEffect->SetTexture("g_SubTex", spTexture->GetTexData()[spTexture->GetMeshIndex()][1]->pTexture);*/
 
 	D3DMATERIAL9* pMtrl = &spGC->m_mtrl;
 
