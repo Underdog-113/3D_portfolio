a774082ae7bb8c3ea9309cd782b41fad2365f5c9
lee <asqqwww1@gmail.com>
2021-06-17 02:48:18 +0900

    theresa sttash

--- Client/Code/Theresa.cpp
+++ Client/Code/Theresa.cpp
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "..\Header\Theresa.h"
+#include "Theresa.h"
 
 
 CTheresa::CTheresa()
@@ -8,5 +8,91 @@ CTheresa::CTheresa()
 
 
 CTheresa::~CTheresa()
+{
+	OnDestroy();
+}
+
+SP(CTheresa) CTheresa::Create(_bool isStatic, Engine::CScene * pScene)
+{
+	SP(CTheresa) spInstance(new CKiana, Engine::SmartDeleter<CTheresa>);
+	spInstance->SetIsStatic(isStatic);
+	spInstance->SetScene(pScene);
+	spInstance->Awake();
+
+	return spInstance;
+}
+
+SP(Engine::CObject) CTheresa::MakeClone(void)
+{
+	SP(CKiana) spClone(new CTheresa, Engine::SmartDeleter<CTheresa>);
+	__super::InitClone(spClone);
+
+	spClone->m_spTransform = spClone->GetComponent<Engine::CTransformC>();
+	spClone->m_spMesh = spClone->GetComponent<Engine::CMeshC>();
+	spClone->m_spGraphics = spClone->GetComponent<Engine::CGraphicsC>();
+	spClone->m_spShader = spClone->GetComponent<Engine::CShaderC>();
+	spClone->m_spTexture = spClone->GetComponent<Engine::CTextureC>();
+
+	spClone->m_spStateMachine = spClone->GetComponent<CFSM_KianaC>();
+	return spClone;
+}
+
+void CTheresa::Awake(void)
+{
+}
+
+void CTheresa::Start(void)
+{
+}
+
+void CTheresa::FixedUpdate(void)
+{
+}
+
+void CTheresa::Update(void)
+{
+}
+
+void CTheresa::LateUpdate(void)
+{
+}
+
+void CTheresa::PreRender(void)
+{
+}
+
+void CTheresa::PreRender(LPD3DXEFFECT pEffect)
+{
+}
+
+void CTheresa::Render(void)
+{
+}
+
+void CTheresa::Render(LPD3DXEFFECT pEffect)
+{
+}
+
+void CTheresa::PostRender(void)
+{
+}
+
+void CTheresa::PostRender(LPD3DXEFFECT pEffect)
+{
+}
+
+void CTheresa::OnDestroy(void)
+{
+}
+
+void CTheresa::OnEnable(void)
+{
+}
+
+void CTheresa::OnDisable(void)
+{
+}
+
+void CTheresa::SetBasicName(void)
 {
 }
---
+++ Client/Header/FSMDefine_Theresa.h
@@ -0,0 +1,91 @@
+#pragma once
+#define Name_VICTORY				L"VICTORY"
+#define Name_Die					L"Die"
+#define Name_STUN					L"STUN"
+#define Name_QTE_FY					L"QTE_FY"
+#define Name_HIT_H					L"HIT_H"
+#define Name_HIT_L					L"HIT_L"
+#define Name_QTE_FY					L"QTE_FY"
+#define Name_JUMP_2					L"JUMP_2"
+#define Name_JUMP_1					L"JUMP_1"
+#define Name_JUMP_0					L"JUMP_0"
+#define Name_SWITCHOUT				L"SWITCHOUT"
+#define Name_SWITCHINATTACK_YD		L"SWITCHINATTACK_YD"
+#define Name_SWITCHINATTACK_TP		L"SWITCHINATTACK_TP"
+#define Name_SWITCHINATTACK_FY		L"SWITCHINATTACK_FY"
+#define Name_SWITCHIN				L"SWITCHIN"
+#define Name_Ult					L"Ult"
+#define Name_CROSSSKILL				L"CROSSSKILL"
+#define Name_CHARGE2				L"CHARGE2"
+#define Name_CHARGE1				L"CHARGE1"
+#define Name_CHARGE0				L"CHARGE0"
+#define Name_ATTACK4				L"ATTACK4"
+#define Name_ATTACK3				L"ATTACK3"
+#define Name_ATTACK2				L"ATTACK2"
+#define Name_ATTACK1				L"ATTACK1"
+#define Name_RUNSTOPRIGHT			L"RUNSTOPRIGHT"
+#define Name_RUNSTOPLEFT			L"RUNSTOPLEFT"
+#define Name_FASTRUN				L"FASTRUN"
+#define Name_RUN					L"RUN"
+#define Name_IDLE					L"IDLE"
+#define Name_APPEAR					L"APPEAR"
+
+#define Index_VICTORY				0
+#define Index_Die					1
+#define Index_STUN					2
+#define Index_QTE_FY				3
+#define Index_HIT_H					4
+#define Index_HIT_L					5
+#define Index_QTE_FY				6
+#define Index_JUMP_2				7
+#define Index_JUMP_1				8
+#define Index_JUMP_0				9
+#define Index_SWITCHOUT				10
+#define Index_SWITCHINATTACK_YD		11
+#define Index_SWITCHINATTACK_TP		12
+#define Index_SWITCHINATTACK_FY		13
+#define Index_SWITCHIN				14
+#define Index_Ult					15
+#define Index_CROSSSKILL			16
+#define Index_CHARGE2				17
+#define Index_CHARGE1				18
+#define Index_CHARGE0				19
+#define Index_ATTACK4				20
+#define Index_ATTACK3				21
+#define Index_ATTACK2				22
+#define Index_ATTACK1				23
+#define Index_RUNSTOPRIGHT			24
+#define Index_RUNSTOPLEFT			25
+#define Index_FASTRUN				26
+#define Index_RUN					27
+#define Index_IDLE					28
+#define Index_APPEAR				29
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
