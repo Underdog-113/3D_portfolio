63f5d4310d231490d8f867c3afa7fc8d0ce7dd03
value95 <ehddlf1223@naver.com>
2021-06-18 17:38:28 +0900

    몬스터 슬라이더 연출 구현중

---
+++ Client/Code/MonsterSlider.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "..\Header\MonsterSlider.h"
+
+
+CMonsterSlider::CMonsterSlider()
+{
+}
+
+
+CMonsterSlider::~CMonsterSlider()
+{
+}
---
+++ Client/Header/MonsterSlider.h
@@ -0,0 +1,8 @@
+#pragma once
+class CMonsterSlider
+{
+public:
+	CMonsterSlider();
+	~CMonsterSlider();
+};
+
