8b15ce2156e9101c51d3a32c2e5f0eb06b80093a
value95 <ehddlf1223@naver.com>
2021-07-17 11:19:46 +0900

    no message

--- Client/Code/BattleUiManager.cpp
+++ Client/Code/BattleUiManager.cpp
@@ -102,7 +102,7 @@ void CBattleUiManager::Start(Engine::CScene * pScene)
 	pScene->FindObjectByName(L"MainCanvas_Image_22")->SetIsEnabled(false);
 	m_vQte.emplace_back(pScene->FindObjectByName(L"MainCanvas_Image_22").get());
 
-	pScene->FindObjectByName(L"MainCanvas_Image_23")->AddComponent<CQteC>()->AddData(1.0f, 1.5f, 5);
+	pScene->FindObjectByName(L"MainCanvas_Image_23")->AddComponent<CQteC>()->AddData(1.0f, 1.5f, 1);
 	pScene->FindObjectByName(L"MainCanvas_Image_23")->SetIsEnabled(false);
 	m_vQte.emplace_back(pScene->FindObjectByName(L"MainCanvas_Image_23").get());
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
